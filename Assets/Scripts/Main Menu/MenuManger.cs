3a032f92297bf3cf887e87cad81461a73f483da4
GabrielZanini <gabrielroszan@gmail.com>
2018-12-01 20:33:24 -0200

    Basic screen flow working.

---
+++ Assets/Scripts/Main Menu/MenuManger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManger : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        PlayerPrefs.SetString("NextScene", "main");
+        SceneManager.LoadSceneAsync("loading");
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadSceneAsync("credits");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
