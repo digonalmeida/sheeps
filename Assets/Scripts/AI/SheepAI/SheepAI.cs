45c359e0156312ed21dd14b54a41cc9e1d6b57b0
Rodrigo Nietiedt de Almeida <rodrigonietiedt@gmail.com>
2018-12-01 03:56:44 -0200

    fsm base

---
+++ Assets/Scripts/AI/SheepAI/SheepAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour
+{
+    private StateMachine stateMachine = new StateMachine();
+    private State idle = new SheepIdleState();
+    
+    public void Awake()
+    {
+        stateMachine.Agent = this;
+    }
+
+    private void Start()
+    {
+        stateMachine.SetState(idle);
+    }
+
+    private void Update()
+    {
+        stateMachine.Update();
+    }
+}
---
+++ Assets/Scripts/AI/SheepAI/SheepIdleState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepIdleState : State
+{
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        var agent = Agent as SheepAI;
+        Debug.Log("entered");
+    }
+
+    public override void OnExit()
+    {
+        base.OnEnter();
+        var agent = Agent as SheepAI;
+        Debug.Log("exit");
+    }
+
+    public override void Update()
+    {
+        base.OnEnter();
+        var agent = Agent as SheepAI;
+        Debug.Log("update");
+    }
+}
