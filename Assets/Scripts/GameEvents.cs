434856781a5e27cc076720d6694fc29da96e090a
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2018-12-01 06:23:54 -0200

    Notification Manager

--- Assets/Scripts/GameEvents.cs
+++ Assets/Scripts/GameEvents.cs
@@ -8,4 +8,8 @@ public static class GameEvents{
     public static class Localization{
         public static Action LanguageChanged;
     }
+
+    public static class Notifications{
+        public static Action<NotificationBlob> TriggerNotification;
+    }
 }
---
+++ Assets/Scripts/NotificationsManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+public class NotificationsManager : Singleton<NotificationsManager>
+{
+    private List<NotificationBlob> notificationHistory = new List<NotificationBlob>();
+
+    public void AddNotification(NotificationBlob blob){
+        notificationHistory.Add(blob);
+        GameEvents.Notifications.TriggerNotification.SafeInvoke(blob);
+    }
+}
+
+public class NotificationBlob
+{
+    private string messageKey;
+
+    public NotificationBlob(string messageKey)
+    {
+        this.messageKey = messageKey;
+    }
+}
