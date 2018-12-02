bfb80fadd4f989f16f73ca7a1c58c2fbe6a47370
GabrielZanini <gabrielroszan@gmail.com>
2018-12-02 12:27:44 -0200

    Better UI. Ending Scene.

---
+++ Assets/Scripts/Ending/EndingManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingManager : MonoBehaviour
+{
+    public List<SheepConfigHolder> sheeps = new List<SheepConfigHolder>();
+    public Sprite dead;
+
+    void Start ()
+    {
+        if (SheepsManager.Instance == null)
+            return;
+
+        KillSheeps();
+    }
+
+    void KillSheeps()
+    {
+        var deadSheep = SheepsManager.Instance.allSheeps.Where(sheep => sheep.isDead).Select(sheepState => sheepState.config);
+
+        for (int i=0; i<sheeps.Count; i++)
+        {
+            if (deadSheep.Contains(sheeps[i].config))
+            {
+                sheeps[i].ChangeSprite(dead);
+            }
+        }        
+    }
+}
