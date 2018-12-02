8b93c92bf593e805e176dfb85e168373100c194a
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-12-02 00:12:43 -0200

    FlipX Animation

--- Assets/Scripts/Sheep/SheepMovementController.cs
+++ Assets/Scripts/Sheep/SheepMovementController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SheepMovementController : MonoBehaviour {
+public class SheepMovementController : MonoBehaviour
+{
     public bool CanMove = false;
     CharacterController charController;
     SheepInputData sheepInputData;
     SpriteRenderer spriteRenderer;
+    SheepAnimationController sheepAnimationController;
     float speed = 5;
     Vector3 gSpeed = new Vector3();
     void Awake()
@@ -14,6 +16,7 @@ public class SheepMovementController : MonoBehaviour {
         charController = GetComponent<CharacterController>();
         sheepInputData = GetComponent<SheepInputData>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        sheepAnimationController = GetComponent<SheepAnimationController>();
     }
 
     // Update is called once per frame
@@ -29,6 +32,11 @@ public class SheepMovementController : MonoBehaviour {
         direction.Normalize();
         gSpeed += Time.deltaTime * Physics.gravity;
         charController.Move((gSpeed * Time.deltaTime) + (direction * Time.deltaTime * sheepInputData.moveSpeed * speed));
+
+        //Flip Sprite
+        if (direction.x <= 0f) sheepAnimationController.setBool("FlippedX", true);
+        else sheepAnimationController.setBool("FlippedX", false);
+
         if (charController.isGrounded)
         {
             gSpeed = Vector3.zero;
