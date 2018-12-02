7c805fc151ef038da7d2b09a2bc0f27a9517e1bd
Thiago Guardado Beltran <thiagoguardado@gmail.com>
2018-12-02 01:56:46 -0200

    Sprites Update

---
+++ Assets/Scripts/TestWolf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestWolf : MonoBehaviour {
+
+
+	private SheepState sheep;
+	public WolfController wolf;
+	public bool die = false;
+	// Use this for initialization
+	void Awake () {
+		sheep = GetComponent<SheepState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(die){
+			wolf.FollowSheep(sheep);
+			die = false;
+		}
+	}
+}
