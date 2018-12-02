a5eb44b1a87d834941165d44f0bd23c13cb832df
GabrielZanini <gabrielroszan@gmail.com>
2018-12-01 22:51:03 -0200

    Localized menu

--- Assets/Scripts/Main Menu/MenuManger.cs
+++ Assets/Scripts/Main Menu/MenuManger.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManger : MonoBehaviour
 {
+    public Button btnEn;
+    public Button btnPt;
+
+    private void Start()
+    {
+        CheckLanguage();
+    }
+
     public void StartNewGame()
     {
         PlayerPrefs.SetString("NextScene", "main");
@@ -19,5 +28,31 @@ public class MenuManger : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void CheckLanguage()
+    {
+        if (LocalizationManager.Instance.GetLanguage() == availableLanguages.enUS)
+        {
+            btnEn.interactable = false;
+            btnPt.interactable = true;
+        }
+        else
+        {
+            btnEn.interactable = true;
+            btnPt.interactable = false;
+        }
+    }
+
+    public void SetLanguageEnUs()
+    {        
+        LocalizationManager.Instance.ChangeLanguage(availableLanguages.enUS);
+        CheckLanguage();
+    }
+
+    public void SetLanguagePtBr()
+    {       
+        LocalizationManager.Instance.ChangeLanguage(availableLanguages.ptBR);
+        CheckLanguage();
     }
 }
