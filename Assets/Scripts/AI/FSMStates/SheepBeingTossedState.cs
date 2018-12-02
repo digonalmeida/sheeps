a0a74135d401272aa5f18c47be876378cb616fd6
Rafael Cauduro Oliveira Macedo <rafael_cauduro@hotmail.com>
2018-12-02 03:28:32 -0200

    Fix Toss

--- Assets/Scripts/AI/FSMStates/SheepBeingTossedState.cs
+++ Assets/Scripts/AI/FSMStates/SheepBeingTossedState.cs
@@ -32,7 +32,7 @@ public class SheepBeingTossedState : FSMState
         }
         else
         {
-            agent.transform.position = Vector3.MoveTowards(agent.transform.position, target, Time.deltaTime * 5f);
+            agent.transform.position = Vector3.MoveTowards(agent.transform.position, target, Time.deltaTime * agent.sheepState.movementSpeed * 2f);
         }
     }
 }
